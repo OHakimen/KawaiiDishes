ca688ddf1107f82e805c98a97722266f573273c5
Lily Autumn <realprismaticyt@gmail.com>
2025-03-18 11:12:02 +1300

    moj moment

--- src/main/java/com/hakimen/kawaiidishes/mixins/EarsTailCompatMixin.java
+++ src/main/java/com/hakimen/kawaiidishes/mixins/EarsTailCompatMixin.java
@@ -2,7 +2,7 @@ package com.hakimen.kawaiidishes.mixins;
 
 import com.hakimen.kawaiidishes.item.armor.TailArmorItem;
 import com.unascribed.ears.common.render.IndirectEarsRenderDelegate;
-import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.player.AbstractClientPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +15,7 @@ public class EarsTailCompatMixin {
     @Inject(method = "isWearingChestplate", at = @At("HEAD"), cancellable = true)
     public void compat(CallbackInfoReturnable<Boolean> cir) {
         IndirectEarsRenderDelegate delegate = (IndirectEarsRenderDelegate) (Object) this;
-        LocalPlayer player = (LocalPlayer) delegate.getPeer();
+        AbstractClientPlayer player = (AbstractClientPlayer) delegate.getPeer();
         if (player.getInventory().getArmor(2).getItem() instanceof TailArmorItem) {
             cir.setReturnValue(false);
         }
