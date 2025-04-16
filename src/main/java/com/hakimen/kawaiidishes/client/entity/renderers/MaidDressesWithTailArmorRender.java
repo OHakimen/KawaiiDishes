f465e7e26e9dbebd4bda44fb5423862b91e7247e
Ampflower 🌺 <git@ampflower.gay>
2025-04-15 21:18:16 -0700

    More Ears support

--- src/main/java/com/hakimen/kawaiidishes/client/entity/renderers/MaidDressesWithTailArmorRender.java
+++ src/main/java/com/hakimen/kawaiidishes/client/entity/renderers/MaidDressesWithTailArmorRender.java
@@ -4,31 +4,39 @@ import com.hakimen.kawaiidishes.KawaiiDishes;
 import com.hakimen.kawaiidishes.client.entity.models.layers.maid_dresses_with_tail_layers.MaidDressOverlayLayer;
 import com.hakimen.kawaiidishes.client.entity.models.layers.maid_dresses_with_tail_layers.PrimaryTailLayer;
 import com.hakimen.kawaiidishes.client.entity.models.layers.maid_dresses_with_tail_layers.SecondaryTailLayer;
+import com.hakimen.kawaiidishes.client.util.EarsSupport;
 import com.hakimen.kawaiidishes.item.armor.MaidDressesWithTailArmorItem;
 import com.hakimen.kawaiidishes.registry.DataComponentRegister;
-import com.hakimen.kawaiidishes.utils.ColorUtils;
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.Nullable;
+import software.bernie.geckolib.cache.object.BakedGeoModel;
 import software.bernie.geckolib.model.GeoModel;
 import software.bernie.geckolib.util.Color;
 
-public class MaidDressesWithTailArmorRender extends GeoArmorItemRenderer<MaidDressesWithTailArmorItem>{
+public class MaidDressesWithTailArmorRender extends GeoArmorItemRenderer<MaidDressesWithTailArmorItem> {
 
     ItemStack stackData;
 
-    public void updateStack(ItemStack stack){
+    float chestAngle;
+
+    public void updateStack(ItemStack stack) {
         this.stackData = stack;
     }
+
     public MaidDressesWithTailArmorRender(GeoModel<MaidDressesWithTailArmorItem> model, ItemStack stack) {
         super(model);
         this.stackData = stack;
 
         addRenderLayer(new MaidDressOverlayLayer(this));
-        addRenderLayer(new PrimaryTailLayer(this, ((MaidDressesWithTailArmorItem)stack.getItem()).getTailType()));
+        addRenderLayer(new PrimaryTailLayer(this, ((MaidDressesWithTailArmorItem) stack.getItem()).getTailType()));
         addRenderLayer(new SecondaryTailLayer(this,((MaidDressesWithTailArmorItem)stack.getItem()).getTailType()));
 
         ((MaidDressOverlayLayer)getRenderLayers().get(0)).updateStack(stack);
@@ -47,11 +55,20 @@ public class MaidDressesWithTailArmorRender extends GeoArmorItemRenderer<MaidDre
     public void prepForRender(@Nullable Entity entity, ItemStack stack, @Nullable EquipmentSlot slot, @Nullable HumanoidModel<?> baseModel) {
         updateStack(stack);
 
-        ((MaidDressOverlayLayer)getRenderLayers().get(0)).updateStack(stack);
-        ((PrimaryTailLayer)getRenderLayers().get(1)).updateStack(stack);
-        ((SecondaryTailLayer)getRenderLayers().get(2)).updateStack(stack);
+        ((MaidDressOverlayLayer) getRenderLayers().get(0)).updateStack(stack);
+        ((PrimaryTailLayer) getRenderLayers().get(1)).updateStack(stack);
+        ((SecondaryTailLayer) getRenderLayers().get(2)).updateStack(stack);
+
+        this.chestAngle = EarsSupport.getChestSize(entity);
 
         super.prepForRender(entity, stack, slot, baseModel);
+    }
+
+    @Override
+    public void preRender(final PoseStack poseStack, final MaidDressesWithTailArmorItem animatable, final BakedGeoModel model, @Nullable final MultiBufferSource bufferSource, @Nullable final VertexConsumer buffer, final boolean isReRender, final float partialTick, final int packedLight, final int packedOverlay, final int colour) {
+        super.preRender(poseStack, animatable, model, bufferSource, buffer, isReRender, partialTick, packedLight, packedOverlay, colour);
+
+        model.getBone("armorChest").orElseThrow().updateRotation(Mth.DEG_TO_RAD * -90f + chestAngle, 0, 0);
     }
 
     @Override
